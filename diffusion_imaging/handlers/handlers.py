10dd646605670a172d552accdb3178667abf32fc
Matthew Jones <boywithstickx@yahoo.com>
2020-01-26 19:17:15 -0800

    Reworked the LocalHandlers class to be extendible for future handler classes

--- diffusion_imaging/handlers/handlers.py
+++ diffusion_imaging/handlers/handlers.py
@@ -20,7 +20,77 @@ class HandlerBase(ABC):
     @abstractmethod
     def load(self):
         pass
+
+class LocalHandler(HandlerBase):
     
+    def __init__(self, patient_directory, label):
+        self.patient_directory = patient_directory
+        self.label = label
+        
+    def _filter(self, file, filters):
+        
+        for filt in filters:
+            if filt in file:
+                return True
+            
+        return False
+        
+    def _get_files(self, path, filters=[]):
+        
+        grouped_file_paths = []
+        files = os.listdir(self.patient_directory)
+        
+        filtered = []
+        for file in files:
+            if len(filters):
+                if not self._filter(file, filters):
+                    filtered.append(os.path.join(self.patient_directory, file))
+            else:
+                filtered.append(os.path.join(self.patient_directory, file))
+        
+        return filtered
+    
+    def _load_dwi(self, file):
+        image = nib.load(file)
+        return image
+    
+    def _make_mri(self, filtered_files):
+
+        # The group is the group associated with the specific 'dir*'
+        # this includes both LR and RL orientations
+        for file in filtered_files:
+            if "bvec" in os.path.basename(file):
+                bvecs_file_path = file
+            elif "bval" in os.path.basename(file):
+                bvals_file_path = file
+            elif os.path.basename(file).endswith('.nii.gz'):
+                dwi_data = self._load_dwi(file)
+                image = dwi_data.get_data()
+                aff = dwi_data.affine
+        
+        # Take the 
+        gtab = gradient_table(bvals_file_path, bvecs_file_path)
+        
+        nifti_image = nib.Nifti1Image(image, aff)
+        
+        mri = build_mri(nifti_image, gtab, self.label)
+        
+        return mri
+    
+    def _make_patient(self, directory):
+        
+        patient = Patient()
+        filtered_files = self._get_files(self.patient_directory)
+        patient.directory = self.patient_directory
+        patient.mri = self._make_mri(filtered_files)
+        
+        return patient
+    
+    def load(self):
+        
+        return self._make_patient(self.patient_directory)
+
+
 class HCPLocalHandler(HandlerBase):
     """
     Class to hanlde the loading of the specific patient files from the Human Connectome Project
@@ -110,93 +180,6 @@ class DMIPYLocalHandler(HCPLocalHandler):
             files_full_dir.append(os.path.join(self.patient_directory, path, file))
 
         return files_full_dir
-
-
-class LocalHandler(HandlerBase):
-    """
-    Class to handle the loading of local files
-    """
-    
-    def __init__(self, config):
-        self.config = config
-        self.dwi = None
-        self.bvec = None
-        self.bval = None
-        self.aff = []
-    
-    def _get_files(self, path, end_extention):
-        
-        out_data = []
-        
-        for file in os.listdir(path):
-            if file.endswith(end_extention) and \
-                (not "SBRef" in file and \
-                 not "BIAS" in file):
-                out_data.append(os.path.join(path, file))
-        
-        return out_data
-    
-    
-    def _load_dwi(self, files):
-        
-        data = []
-        aff = []
-        
-        for file in files:
-            loaded = nib.load(file)
-            data.append(loaded.get_data())
-            
-        aff = loaded.affine
-        
-        return (data, aff)
-    
-    
-    def _load_bvec(self, files):
-        
-        data = []
-        for file in files:
-            data.append(np.loadtxt(file))
-        
-        return data
-    
-    
-    def _load_bval(self, files):
-        
-        data = []
-        for file in files:
-            data.extend(np.loadtxt(file))
-        
-        return data
-    
-    
-    def _get_data(self, path, end_extention):
-        
-        switch = {
-            ".nii.gz": self._load_dwi,
-            ".bvec": self._load_bvec,
-            ".bval": self._load_bval
-        }
-        
-        paths = self._get_files(path, end_extention)
-        data = switch[end_extention](paths)
-        
-        return data
-        
-    
-    def load(self):
-        
-        dwi_path = self.config['dwi_path']
-        bvec_path = self.config['bvec_path']
-        bval_path = self.config['bval_path']
-        
-        dwi, af = self._get_data(dwi_path, ".nii.gz")
-        bvec = self._get_data(bvec_path, ".bvec")
-        bval = self._get_data(bval_path, ".bval")
-        
-        out = [nib.Nifti1Image(np.concatenate(dwi, -1), af),
-               gradient_table(bval, np.concatenate(bvec, -1))]
-        
-        return out
 
 
 Handler = providers.FactoryAggregate(local=providers.Factory(LocalHandler))
